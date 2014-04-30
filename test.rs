f7cfb2603c471395ca2f1c102e7b42131df391db
Allie Wood <allisonwood94@gmail.com>
2014-04-29 21:53:35 -0400

    Changed test

--- test.rs
+++ test.rs
@@ -1,10 +1,23 @@
+#![feature(macro_rules)]
 extern crate opencl = "OpenCL#0.2";
 
 use opencl::hl;
 
+macro_rules! expect (
+    ($test: expr, $expected: expr) => ({
+            let test     = $test;
+            let expected = $expected;
+            if test != expected {
+                fail!(format!("Test failure in {:s}: expected {:?}, got {:?}",
+                              stringify!($test),
+                              expected, test))
+            }
+        })
+        )
+
 fn main () {
-    let src = "__kernel void test(__global int *i, long int k) { \
-                   *i += k; \
+    /*let src = "__kernel void test(__global int *i, long int k) {
+                   *i += k;
                    }";
     let prog = ctx.create_program_from_source(src);
     prog.build(&device).unwrap();
@@ -16,15 +29,20 @@ fn main () {
     queue.enqueue_async_kernel(&k, 1, None, ()).wait();
     let v: ~[int] = queue.get(&v, ());
 
+    expect!(v[0], 43);*/
     
     let platforms = hl::get_platforms();
     for p in platforms.iter() {
+        println!("platform found");
+        println!("platform: {}", p.name());
         let devices = p.get_devices();
+        println!("got the devices!!")
         for d in devices.iter() {
+            println!("device found");
             let context = d.create_context();
             let queue = context.create_command_queue(d);
             let name = d.name();
-            println!("device found");
+            println!("done with the device");
         }
     }
     
