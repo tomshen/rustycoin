0056228b7232830d40d5a615cb0d482add01d99a
Tom Shen <tom@shen.io>
2014-04-30 23:23:42 -0400

    Switch to mpz_class

--- primes.cpp
+++ primes.cpp
@@ -3,57 +3,48 @@
 #include <string.h>
 #include <vector>
 
-#include <gmp.h>
+#include <gmpxx.h>
 
 #include "primes.h"
 #include "util.h"
 
 static uint32_t cluster_offsets[6] = {0, 4, 2, 4, 2, 4};
 
-bool is_even(mpz_t n) {
-  return mpz_divisible_ui_p(n, 2) != 0;
+bool is_even(mpz_class n) {
+  return mpz_divisible_ui_p(n.get_mpz_t(), 2) != 0;
 }
 
-bool is_prime_fermat(mpz_t n, int k) {
-  mpz_t one, two, rem, nmo, a;
+bool is_prime_fermat(mpz_class n, int k=100) {
+  mpz_class one, two, rem, nmo, a;
   gmp_randstate_t seed;
 
-  mpz_inits(one, two, rem, nmo, a, NULL);
   gmp_randinit_default(seed);
 
-  mpz_set_ui(one, 1);
-  mpz_set_ui(two, 2);
+  one = 1;
+  two = 2;
 
-  if (mpz_cmp(n, two) == 0) {
+  if (n == 2) {
     return true;
   } else if (is_even(n)) {
     return false;
   }
 
-  mpz_sub(nmo, n, one);
+  nmo = n - one;
   for (int i = 0; i < k; i++) {
-    mpz_urandomm(a, seed, n);
-    mpz_powm(rem, a, nmo, n);
-    if (mpz_cmp(rem, one) != 0)
+    mpz_urandomm(a.get_mpz_t(), seed, n.get_mpz_t());
+    mpz_powm(rem.get_mpz_t(), a.get_mpz_t(), nmo.get_mpz_t(), n.get_mpz_t());
+    if (rem != one)
       return false;
   }
   return true;
 }
 
-bool is_valid_pow(mpz_t candidate) {
-
-  unsigned int offsets [5] = {4, 2, 4, 2, 4};
-  
-  mpz_t offset;
-  mpz_init(offset);
-  for (int i = 0; i < 5; i++) {
-    mpz_set_ui(offset, offsets[i]);
-    
-    mpz_add(candidate, candidate, offset);
-    
-    if (!is_prime_fermat(candidate)) return false;
+bool is_valid_pow(mpz_class candidate) {
+  for (int i = 0; i < 6; i++) {
+    candidate += cluster_offsets[i];
+    if (!is_prime_fermat(candidate))
+      return false;
   }
-  
   return true;
 }
 
@@ -62,90 +53,87 @@ void sieve(uint32_t** prime_test_table, uint32_t* prime_test_size,
   /* Source: http://git.io/-m1Ypw
    * We use uint8 instead of bool because otherwise our array would be too
    * large to index into. */
-  *prime_test_table = (uint32_t*)malloc(sizeof(uint32_t)*(prime_test_limit/4+10));
+  *prime_test_table = (uint32_t*)malloc(sizeof(uint32_t)
+      *(prime_test_limit/4+10));
   if (prime_test_table == NULL) {
     perror("could not allocate prime test table");
     exit(-1);
   }
   *prime_test_size = 0;
 
-  uint8_t* vfComposite = (uint8_t*)malloc(sizeof(uint8_t)*(prime_test_limit+7)/8);
+  uint8_t* vfComposite = (uint8_t*)malloc(sizeof(uint8_t)
+      *(prime_test_limit+7)/8);
   if (vfComposite == NULL) {
     perror("could not allocate vfComposite table");
     exit(-1);
   }
   memset(vfComposite, 0x00, sizeof(uint8_t)*(prime_test_limit+7)/8);
-  for (unsigned int nFactor = 2; nFactor * nFactor < prime_test_limit; nFactor++)
-  {
+  for (unsigned int nFactor = 2; nFactor * nFactor < prime_test_limit;
+      nFactor++) {
     if(vfComposite[nFactor>>3] & (1<<(nFactor&7)))
       continue;
     for (unsigned int nComposite = nFactor * nFactor;
         nComposite < prime_test_limit; nComposite += nFactor)
       vfComposite[nComposite>>3] |= 1<<(nComposite&7);
   }
-  for (unsigned int n = 2; n < prime_test_limit; n++)
-  {
-    if ((vfComposite[n>>3] & (1<<(n&7))) == 0)
-    {
+  for (unsigned int n = 2; n < prime_test_limit; n++) {
+    if ((vfComposite[n>>3] & (1<<(n&7))) == 0) {
       (*prime_test_table)[*prime_test_size] = n;
       (*prime_test_size)++;
     }
   }
   *prime_test_table = (uint32_t*)realloc(*prime_test_table,
-      sizeof(uint32_t)*(*prime_test_size));
+    sizeof(uint32_t)*(*prime_test_size));
   free(vfComposite);
 }
 
-bool candidate_killed_by(mpz_t candidate, mpz_t prime) {
-  mpz_t p, modp;
-  mpz_init_set(p, candidate);
+bool candidate_killed_by(mpz_class candidate, mpz_class prime) {
+  mpz_class p;
+  p = candidate;
   for(int i = 0; i < 6; i++) {
-    mpz_add_ui(p, p, cluster_offsets[i]);
-    mpz_mod(modp, p, prime);
-    if (mpz_cmp_ui(modp, 0) == 0)
+    p += cluster_offsets[i];
+    if (p % prime == 0)
       return true;
   }
   return false;
 }
 
 
-std::vector<mpz_t> add_next_prime(std::vector<mpz_t> offsets, mpz_t max_val,
-    mpz_t prime, mpz_t primorial) {
-  mpz_t base, counter, val;
-  mpz_inits(base, counter, NULL);
-  mpz_set_ui(base, 0);
-  mpz_set_ui(counter, 0);
+std::vector<mpz_class> add_next_prime(std::vector<mpz_class> offsets,
+    mpz_class max_val, mpz_class prime, mpz_class primorial) {
+  mpz_class base, counter, val;
+  base = 0;
+  counter = 0;
 
-  std::vector<mpz_t> new_offsets;
+  std::vector<mpz_class> new_offsets;
 
-  while (mpz_cmp(counter, prime) < 0) {
-    if (mpz_cmp(base, max_val) > 0)
+  while (counter < prime) {
+    if (base > max_val)
       break;
-    for (std::vector<mpz_t>::iterator o = offsets.begin(); o != offsets.end();
-        o++) {
-      mpz_init(val);
-      mpz_add(val, base, *o);
-      if (mpz_cmp(val, max_val) > 0)
+    for (std::vector<mpz_class>::iterator o = offsets.begin();
+        o != offsets.end(); o++) {
+      val = base + *o;
+      if (val > max_val)
         break;
       if (!candidate_killed_by(val, prime))
         new_offsets.push_back(val);
     }
-    mpz_add(base, base, primorial);
-    mpz_add_ui(counter, counter, 1u);
+    base = base + primorial;
+    counter += 1;
   }
 
   return new_offsets;
 }
 
-uint32_t generate_prime_clusters(mpz_t max_val, uint32_t max_sieve, bool verbose) {
+uint32_t generate_prime_clusters(mpz_class max_val, uint32_t max_sieve,
+    bool verbose) {
   uint32_t primorial_start = 7;
 
-  mpz_t primorial, big97, prime;
-  mpz_inits(primorial, big97, prime, NULL);
-  mpz_set_ui(primorial, 210);
-  mpz_set_ui(big97, 97);
+  mpz_class primorial, big97, prime;
+  primorial = 210;
+  big97 = 97;
 
-  std::vector<mpz_t> offsets;
+  std::vector<mpz_class> offsets;
   offsets.push_back(big97);
 
   uint32_t *prime_test_table;
@@ -153,13 +141,13 @@ uint32_t generate_prime_clusters(mpz_t max_val, uint32_t max_sieve, bool verbose
   sieve(&prime_test_table, &prime_test_size, max_sieve);
 
   for (uint32_t i = primorial_start+1; i < prime_test_size; i++) {
-    mpz_set_ui(prime, i);
+    prime = i;
     offsets = add_next_prime(offsets, max_val, prime, primorial);
-    mpz_mul_ui(primorial, primorial, i);
+    primorial = primorial * i;
   }
   uint32_t count = 0;
-  for (std::vector<mpz_t>::iterator o = offsets.begin(); o != offsets.end();
-      o++) {
+  for (std::vector<mpz_class>::iterator o = offsets.begin();
+      o != offsets.end(); o++) {
     if (is_valid_pow(*o)) {
       count++;
       if (verbose)
--- tests.cpp
+++ tests.cpp
@@ -22,22 +22,24 @@ void test_sieve() {
   uint32_t prime_sieve_max = 900000000;
   sieve(&prime_test_table, &prime_test_size, prime_sieve_max);
   uint32_t exp_size = 46009215u;
-  expect(exp_size, prime_test_size, "Prime sieve doesn't find correct number of primes");
+  expect(exp_size, prime_test_size,
+      "Prime sieve doesn't find correct number of primes");
   free(prime_test_table);
 }
 
 void test_is_valid_pow() {
   mpz_t input;
   mpz_init(input);
-  
+
   mpz_set_ui(input, 97);
   expect(true, is_valid_pow(input), "POW checker gives false negative on 97");
-  
+
   mpz_set_ui(input, 103);
   expect(false, is_valid_pow(input), "POW checker gives false positive on 103");
 
   mpz_set_ui(input, 14812867);
-  expect(true, is_valid_pow(input), "POW checker gives false negative on 14812867");
+  expect(true, is_valid_pow(input),
+      "POW checker gives false negative on 14812867");
 }
 
 
